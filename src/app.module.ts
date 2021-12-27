332e3f69e6de3fa01fe8a35bffe03284ebfc2bcf
Grisha Poghosyan <grisha.poghosyan@softconstruct.com>
2021-12-27 23:10:12 +0400

    refactor: removed unused controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
