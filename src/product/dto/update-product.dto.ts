c9d7eb0246c6975abdc09f9b0ac3ccd5d1a16b0a
Grisha Poghosyan <grisha.poghosyan@softconstruct.com>
2022-01-05 15:43:24 +0400

    code formatted. Product update validators message

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,13 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { Type } from 'class-transformer';
 import { IsArray, IsNumber, IsOptional } from 'class-validator';
+import { ValidationHelpers } from 'src/common/helpers/ValidationHelpers';
+
+import { PartialType } from '@nestjs/mapped-types';
 
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
   @IsOptional()
-  @IsArray()
+  @IsArray({
+    message: ValidationHelpers.compileValueErrorMessage('an array'),
+  })
   @Type(() => Number)
-  @IsNumber({}, { each: true })
+  @IsNumber(
+    {},
+    {
+      each: true,
+      message: ValidationHelpers.compileValueErrorMessage('a number element'),
+    },
+  )
   public deletedImages: Array<number>;
 }
